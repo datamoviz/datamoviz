a5bd1e08665a6f6de16bf892c2f049c3cca62ba6
Quentin de Longraye <quentin@dldl.fr>
2017-11-05 19:41:23 +0100

    add c3js with example chart

--- client/app/js/module/overview.js
+++ client/app/js/module/overview.js
@@ -1,13 +1,25 @@
+import c3 from 'c3'
+
 /**
  * This module is in charge to handle the rendering of the overall movies
  * section.
  */
 export default class OverviewModule {
   constructor(selector) {
-    this.section = document.querySelector(selector);
+    this.selector = selector;
   }
 
   render() {
-    this.section.innerHTML = 'Hello world!';
+    c3.generate({
+        bindto: this.selector,
+        data: {
+          columns: [
+            ['Evolution of movies budget', 30, 200, 100, 400, 150, 250]
+          ],
+          colors: {
+            'Evolution of movies budget': '#009946'
+          }
+        }
+    });
   }
 }
--- client/app/main.js
+++ client/app/main.js
@@ -1,6 +1,7 @@
 import * as d3 from 'd3';
 import 'bootstrap-reboot/bootstrap-reboot.scss';
 import 'bootstrap-4-grid/scss/grid.scss';
+import 'c3/src/scss/main.scss';
 import './scss/main.scss';
 
 import HomeworkModule from './js/module/homework'
@@ -8,7 +9,7 @@ import BoilerplateModule from './js/module/homework'
 import OverviewModule from './js/module/overview'
 
 let homework = new HomeworkModule('#colors', 'https://upload.wikimedia.org/wikipedia/commons/0/05/Odd-eyed_cat_by_ihasb33r.jpg');
-let movies = new OverviewModule('section#overview')
+let overview = new OverviewModule('#overview-chart')
 
 homework.render();
 overview.render();
