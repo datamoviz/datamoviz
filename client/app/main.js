c2b92b72365fd59e9e79aa54b14ff8c2033ab26f
Quentin de Longraye <quentin@dldl.fr>
2017-11-07 14:03:29 +0100

    add header count

--- client/app/main.js
+++ client/app/main.js
@@ -4,12 +4,15 @@ import 'bootstrap-4-grid/scss/grid.scss';
 import 'c3/src/scss/main.scss';
 import './scss/main.scss';
 
-import HomeworkModule from './js/module/homework'
-import BoilerplateModule from './js/module/homework'
-import OverviewModule from './js/module/overview'
+import HomeworkModule from './js/module/homework';
+import BoilerplateModule from './js/module/homework';
+import OverviewModule from './js/module/overview';
 
-let homework = new HomeworkModule('#colors', 'https://upload.wikimedia.org/wikipedia/commons/0/05/Odd-eyed_cat_by_ihasb33r.jpg');
-let overview = new OverviewModule('#overview-chart')
+const modules = [
+  new HomeworkModule('#colors', 'https://upload.wikimedia.org/wikipedia/commons/0/05/Odd-eyed_cat_by_ihasb33r.jpg'),
+  new OverviewModule('#overview-chart')
+];
 
-homework.render();
-overview.render();
+modules.forEach(module => {
+  module.render();
+})
