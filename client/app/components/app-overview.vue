6ef54f1a2a7f635af9125b6154cb327a3c18a6e0
Quentin de Longraye <quentin@dldl.fr>
2017-11-14 17:09:51 +0100

    overview chart is working

--- client/app/components/app-overview.vue
+++ client/app/components/app-overview.vue
@@ -10,41 +10,67 @@
 </template>
 
 <script>
+  import { FILTERS_UPDATE } from '../event-bus';
   import c3 from 'c3';
   import * as d3 from 'd3';
+
+  let chart;
 
   export default {
     name: 'app-overview',
     methods: {
+      loadCount(filters) {
+        filters = filters || {};
+        fetch(`${process.env.SERVER_URL}/aggregate/movies?filters=${encodeURI(JSON.stringify(filters))}`)
+          .then(response => response.json())
+          .then((years) => {
+            chart.load({
+              json: years,
+              keys: {
+                x: '_id',
+                value: ['count']
+              }
+            });
+          });
+      },
       loadChart() {
-        c3.generate({
+        chart = c3.generate({
           bindto: this.$refs.overviewChart,
           data: {
-            x: 'Years',
-            columns: [
-              ['Years', ...[...new Array(117).keys()].map((val) => 1900 + val)],
-              ['Movies average budget', ...[...new Array(117).keys()].map((val) => 1000000 + Math.round((val + Math.random()*20)**4))],
-              ['Movies average revenues', ...[...new Array(117).keys()].map((val) => 1000000 + Math.round((val + Math.random()*20)**4.1))]
-            ]
-          },
-          color: {
-            pattern: ['#009946', '#2f76b5']
-          },
-          axis : {
-            y: {
-              tick: {
-                format: d3.format("$,")
-              }
+            json: [],
+            type: 'bar',
+            colors: {
+              count: '#009946'
             }
           },
           subchart: {
             show: true
+          },
+          axis: {
+            y: {
+              tick: {
+                values: [...[...new Array(10).keys()].map((val) => 1000*val)]
+              }
+            }
+          },
+          grid: {
+            x: {
+              show: true,
+            },
+            y: {
+              show: true
+            }
           }
         });
       }
     },
     mounted() {
+      this.loadCount();
       this.loadChart();
+
+      this.$bus.$on(FILTERS_UPDATE, (filters) => {
+        this.loadCount(filters);
+      });
     }
   };
 </script>
--- server/src/services/customs/aggregate.js
+++ server/src/services/customs/aggregate.js
@@ -2,17 +2,35 @@ module.exports = function (app) {
 
   app.use('/aggregate/movies', {
     find(request) {
-      let rules;
-      if (request.query.rules === undefined) {
-        rules = [];
+      let filters;
+      if (request.query.filters === undefined) {
+        filters = {};
       } else {
-        rules = JSON.parse(decodeURI(request.query.rules));
+        filters = JSON.parse(decodeURI(request.query.filters));
       }
 
       return app.get('mongoClient').then(db => {
-        return db.collection('movies').aggregate(rules, (err, res) => {
-          return res;
-        });
+        return db.collection('movies').aggregate([
+          {
+            $match: filters
+          },
+          {
+            $project: {
+              year: { $year: ['$release_date'] }
+            }
+          },
+          {
+            $group: {
+              _id: '$year',
+              count: { $sum: 1 }
+            }
+          },
+          {
+            $sort: {
+              _id: -1
+            }
+          }
+        ]).toArray()
       });
     }
   });
