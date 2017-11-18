563f89f66b67267fa587e7a4f648d9544ab0e6ca
Coac <levictor3000@hotmail.fr>
2017-11-18 18:57:48 +0100

    Network endpoint working

--- server/src/services/customs/network.js
+++ server/src/services/customs/network.js
@@ -3,19 +3,76 @@ module.exports = function (app) {
   app.use('/network', {
     async find() {
       const db = await app.get('mongoClient');
-      const movies = await db.collection('movies').find().limit(2).toArray();
+      const movies = await db.collection('movies').find().limit(100).toArray();
 
       const moviesIds = collectMoviesIds(movies);
-      console.log(moviesIds)
 
-      const credits = await db.collection('credits').find({ id: { $in:moviesIds } }).toArray();
-      console.log(credits)
+      const credits = await db.collection('credits').find({ id: { $in:moviesIds } }, { cast: 1, id: 1 }).toArray();
 
+      const actors = await db.collection('credits').distinct('cast.name', { id: { $in:moviesIds } });
 
-      return movies
+      const network = getActorsNetworkLink(credits, actors);
+
+      return {actors, network}
     }
   });
 };
+
+function getActorsLinkMap(credits, actors) {
+  const actorsLink = new Map();
+  actors.forEach(actor => {
+    credits.forEach(movieCast => {
+
+      if(!castContainActor(movieCast.cast, actor)) {
+        return;
+      }
+
+      movieCast.cast.forEach(actorObject2 => {
+        if(actor === actorObject2.name) {
+          return;
+        }
+
+        // Keep only (actorname1, actorname2) and throw (actorname2, actorname1)
+        if(actor.localeCompare(actorObject2.name) > 0) {
+          return;
+        }
+
+        const linkObject = {
+          actor1: actor,
+          actor2: actorObject2.name
+        }
+
+        const key = JSON.stringify(linkObject);
+
+        const existingCount = actorsLink.get(key);
+        if(existingCount) {
+          actorsLink.set(key, existingCount + 1);
+        } else {
+          actorsLink.set(key, 1);
+        }
+
+      })
+
+    })
+
+  });
+
+  return actorsLink;
+}
+
+function getActorsNetworkLink(credits, actors) {
+  const actorsLinkMap = getActorsLinkMap(credits, actors);
+
+  const links = [];
+
+  actorsLinkMap.forEach((count, strLink) => {
+    const linkObject = JSON.parse(strLink);
+    linkObject.count = count;
+    links.push(linkObject);
+  })
+
+  return links;
+}
 
 function collectMoviesIds(movies) {
   let moviesIds = [];
@@ -25,4 +82,16 @@ function collectMoviesIds(movies) {
   });
 
   return moviesIds;
+}
+
+function castContainActor(cast, actor) {
+  let found = false;
+  cast.forEach(actorObject => {
+    if(actorObject.name === actor) {
+      found = true;
+      return false;
+    }
+  })
+
+  return found;
 }
