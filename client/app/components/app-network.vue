db45cc133f17bce5058b4a04204a9f282b41f5bf
Coac <levictor3000@hotmail.fr>
2017-11-28 13:27:52 +0100

    Better actor network update

--- client/app/components/app-network.vue
+++ client/app/components/app-network.vue
@@ -93,21 +93,9 @@
           .text(d => d.name)
           .merge(this.text);
 
-        this.simulation
-          .nodes(this.graph.actors)
-          .on('tick', () => {
-            this.link
-              .attr('x1', d => d.source.x)
-              .attr('y1', d => d.source.y)
-              .attr('x2', d => d.target.x)
-              .attr('y2', d => d.target.y);
-
-            this.node.attr('transform', d => `translate(${d.x},${d.y})`);
-            this.text.attr('transform', d => `translate(${d.x},${d.y})`);
-          });
-
+        this.simulation.nodes(this.graph.actors);
         this.simulation.force('link').links(this.graph.links);
-        // this.simulation.alpha(1).restart();
+        this.simulation.alpha(1).restart();
       },
       drawGraph(svg) {
         const color = d3.scaleOrdinal(d3.schemeCategory20);
@@ -115,6 +103,26 @@
         this.link = svg.append("g").attr("stroke", "white").attr("stroke-width", 1.5).selectAll(".link")
         this.node = svg.append("g").attr("stroke", "#fff").attr("stroke-width", 1.5).selectAll(".node");
         this.text = svg.append('g').attr('class', 'texts').selectAll('.text')
+
+        const width = +svg.attr('width');
+        const height = +svg.attr('height');
+
+        this.simulation = d3.forceSimulation(this.graph.actors)
+          .force('link', d3.forceLink(this.graph.links).id(d => d.name).distance(5))
+          .force('charge', d3.forceManyBody().strength(-300))
+          .force('center', d3.forceCenter(width / 2, height / 2))
+          .force("x", d3.forceX())
+          .force("y", d3.forceY())
+          .on('tick', () => {
+            this.link
+              .attr('x1', d => d.source.x)
+              .attr('y1', d => d.source.y)
+              .attr('x2', d => d.target.x)
+              .attr('y2', d => d.target.y);
+
+              this.node.attr('transform', d => `translate(${d.x},${d.y})`);
+              this.text.attr('transform', d => `translate(${d.x},${d.y})`);
+            });
 
         this.updateGraph()
       }
@@ -128,14 +136,6 @@
       });
 
       const svg = d3.select(this.$refs.actorsNetwork).attr('class', 'actors-network');
-      const width = +svg.attr('width');
-      const height = +svg.attr('height');
-
-      this.simulation = d3.forceSimulation()
-        .force('link', d3.forceLink().id(d => d.name))
-        .force('charge', d3.forceManyBody())
-        .force('center', d3.forceCenter(width / 2, height / 2));
-
 
       this.loadGraphData().then(() => {
         this.drawGraph(svg);
