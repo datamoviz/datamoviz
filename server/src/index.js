43ac277f9ed8208bd445bf8eb9bdc71accfbee5c
Quentin de Longraye <quentin@dldl.fr>
2018-05-21 11:26:57 +0200

    openshift ports

--- server/src/index.js
+++ server/src/index.js
@@ -23,5 +23,9 @@ require('./network')(app, router);
 
 app.use('/api', router);
 
-const port = process.env.PORT || 3030;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.listen(port, ip);
+
+module.exports = app;
